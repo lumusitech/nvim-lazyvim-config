16f3226f9c706fa3c0fb3a5e58a909029b28891c
Luciano Figueroa <lumusitech@gmail.com>
2026-02-05 06:09:54 +0000

    feat: add global clipboard between windows and lazygit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,22 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Habilitar el portapapeles del sistema
+vim.opt.clipboard = "unnamedplus"
+
+-- Configuración específica para WSL2 usando win32yank
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
